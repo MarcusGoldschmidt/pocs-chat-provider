0bfe4e3801b6dbd3c572ca1be05ad5f80fe03bf0
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-02-13 11:42:26 -0300

    Configurado Socket IO

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+    });
+    res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
