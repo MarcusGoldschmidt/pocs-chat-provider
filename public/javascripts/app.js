94fbb6cff3a094fede5b45691476eb0327440293
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-02-26 10:57:59 -0400

    Alterado envio de mensagem

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,7 +3,11 @@ const socket = io();
 
 (function (window) {
     const msg = document.getElementById('msg');
-    msg.addEventListener('keyup', function () {
-        socket.emit('chat message', msg.value);
+    msg.addEventListener('keyup', function (e) {
+        if(e.key === "Enter"){
+            socket.emit('chat message', msg.value);
+        }else{
+            socket.emit('chat message', "Digitando...");
+        }
     });
 }(this));
