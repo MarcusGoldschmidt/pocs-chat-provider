2e9edf1a7dc870be5954bfb1598180fb0f2bf463
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-02-26 10:57:37 -0400

    Reajustado mensagem

--- socket.js
+++ socket.js
@@ -2,9 +2,9 @@ var io = {};
 module.exports = (server, serveradmin) => {
     io = require('socket.io')(server);
     io.on('connection', function (socket) {
-        console.log('[[[[[[[[[ A user connected');
+        console.log('A user connected');
         socket.on('chat message', function (msg) {
-            console.log('[[[[[[[[[ Message: ' + msg);
+            console.log('Message: ' + msg);
         });
     });
 };
